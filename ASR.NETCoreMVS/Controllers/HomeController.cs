02f700b25eb7b9c7a8758c36b823fa8847068c2d
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-05-22 19:49:52 +0500

    добавить модел Person, подключить к базу, отправить модел в представления finish

--- ASR.NETCoreMVS/Controllers/HomeController.cs
+++ ASR.NETCoreMVS/Controllers/HomeController.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using ASR.NETCoreMVS.Models.Persons;
 
 namespace ASR.NETCoreMVS.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index(int? Id)
-        {
-            return View();
+        { 
+            return View(new ConnectionDataPersons().GetInfoPersons());
         }
     }
 }
---
+++ ASR.NETCoreMVS/Models/Persons/ConnectionDataPersons.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ASR.NETCoreMVS.Models.Persons
+{
+    public class ConnectionDataPersons
+    {
+        public string ConnString = "Data Source=Amirbek;Initial Catalog=AlifAcademy;Integrated Security=True";
+        public List<Person> GetInfoPersons()
+        {
+            using (IDbConnection db = new SqlConnection(ConnString))
+            {
+                return db.Query<Person>("SELECT * FROM PERSON").ToList<Person>();
+            }
+        }
+    }
+}
